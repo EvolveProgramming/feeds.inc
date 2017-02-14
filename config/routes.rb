5ccfd37835a9335fba186c508b965a49c2068ef0
Evans-Munatsa <gagamel.em@gmail.com>
2017-02-14 15:12:26 +0200

    feeds added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root 'feeds#index'
+  resources :feeds do
+    member do
+      resources :entries, only: [:index, :show]
+    end
+  end
+
   get 'static_pages/home'
-
   get 'static_pages/about'
-
   get 'static_pages/contact'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/views/entries/index.html.erb_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe "entries/index.html.erb", type: :view do
+end
