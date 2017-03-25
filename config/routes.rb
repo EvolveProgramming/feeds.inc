a0e98d07470896694d2553fd3871add4e103f116
R2D2-12 <bsccomputerscience9@gmail.com>
2017-03-25 15:00:03 +0200

    WIP - complaints section - added routing to navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
-  get 'complaints/index'
 
   devise_for :users
+  
   root 'static_pages#index'
-
-  get 'static_pages/home'
-  get 'static_pages/feeds'
-  get 'static_pages/complaints'
   get 'static_pages/contact'
+
 
   resources :feeds do
     member do
       resources :entries, only: [:index, :show]
     end
   end
+
+  resources :complaints
 
   resources :contacts do
     collection { post :import }
